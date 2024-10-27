2e10cd17d74eeaf90b3e5628dbf29779d1f5415f
ruirui3 <ruiruithing@gmail.com>
2024-10-27 08:40:47 -0700

    Setup Collinear Points

---
+++ LineSegment.java
@@ -0,0 +1,14 @@
+public class LineSegment {
+    // constructs the line segment between points p and q
+    public LineSegment(Point p, Point q) {
+
+    }        
+    // draws this line segment
+    public void draw() {
+
+    }       
+    // string representation                 
+    public String toString() {
+
+    }                
+}
---
+++ Point.java
@@ -0,0 +1,30 @@
+import java.util.Comparator;
+
+public class Point implements Comparable<Point> {
+    
+    public int compareTo(Point that) {
+
+    }
+
+    public double slopeTo(Point that) {
+
+    }
+    public Comparator<Point> slopeOrder() {
+
+    }
+
+    // draws this point
+    public void draw() {
+
+    }    
+
+    // draws the line segment from this point to that point                       
+    public void drawTo(Point that) {
+
+    }   
+      
+    // string representation              
+    public String toString() {
+
+    } 
+}
