6349a49232a40285e629dd15997ac986448caa02
ruirui3 <ruiruithing@gmail.com>
2024-10-27 10:21:15 -0700

    um 3

--- BruteCollinearPoints.java
+++ BruteCollinearPoints.java
@@ -1,12 +1,20 @@
+import java.util.Arrays;
+
 public class BruteCollinearPoints {
+
+    private int segments = 0;
+
+
     // finds all line segments containing 4 points
     public BruteCollinearPoints(Point[] points) {
+        
+        Arrays.sort(points);
 
     }  
     
     // the number of line segments
-    public           int numberOfSegments() {
-
+    public int numberOfSegments() {
+        return segments;
     }    
     
     // the line segments
