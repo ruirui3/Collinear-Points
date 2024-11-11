c657ad10de6a435ac292f0e89900a9badea16770
ruirui3 <ruiruithing@gmail.com>
2024-11-11 11:40:50 -0800

    I think finish with brute? (need test)

--- BruteCollinearPoints.java
+++ BruteCollinearPoints.java
@@ -1,21 +1,25 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class BruteCollinearPoints {
 
     private int segments = 0;
-
-
+    private ArrayList<LineSegment> lineSegment;
+    private LineSegment[] listedLines;
     // finds all line segments containing 4 points
     public BruteCollinearPoints(Point[] points) {
         
         Arrays.sort(points);
+        lineSegment = new ArrayList<LineSegment>();
         for (int i = 0; i<points.length-3; i++) {
             for (int j = i+1; j<points.length-2; j++) {
                 for (int k = j+1; k<points.length-1; k++) {
                     for (int m = k+1; m<points.length; m++) {
 
                         if (points[i].slopeTo(points[j]) == points[j].slopeTo(points[k]) && points[j].slopeTo(points[k]) == points[k].slopeTo(points[m])) {
-
+                            
+                            lineSegment.add(new LineSegment(points[i], points[m])); // list a possible slope connection
+                            segments++;
                         }
 
                     }
@@ -32,5 +36,12 @@ public class BruteCollinearPoints {
     // the line segments
     public LineSegment[] segments() {
         
+        listedLines = new LineSegment[lineSegment.size()];
+        for (int i = 0; i<lineSegment.size(); i++) {
+            listedLines[i] = lineSegment.get(i);
+        }
+        return listedLines;
+
+
     }  
 }
--- FastCollinearPoints.java
+++ FastCollinearPoints.java
@@ -1,8 +1,13 @@
+import java.util.Arrays;
+
 public class FastCollinearPoints {
 
     // finds all line segments containing 4 or more points
     public FastCollinearPoints(Point[] points) {
-
+        Arrays.sort(points);
+        for (int i = 0; i<points.length; i++) {
+            
+        }
     }     
 
     // the number of line segments
